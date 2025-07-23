4fee72b1629890fd5f935a15dbe0dd4fb3082fbe
AjayTagilepally11 <136341767+AjayTagilepally11@users.noreply.github.com>
2025-07-23 12:19:31 +0530

    commit

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -9,7 +9,7 @@ import { FiDownload } from "react-icons/fi";
 import { FaPlay } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
 // import resumePDF from "../../assets/Ajay_Resume.pdf";
-import resumePDF from "src/assets/Ajay_Resume.pdf"
+import resumePDF from "src/assets/Ajay_Resume.pdf";
 
 
 const Hero = () => {
